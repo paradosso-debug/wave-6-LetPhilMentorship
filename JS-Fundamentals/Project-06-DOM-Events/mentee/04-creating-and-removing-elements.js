566fa8b7f51c81b8d9a4959a4a7ac804f4aa6d34
Jocelin Arriaga <jocelin.arriaga@yahoo.com>
2026-01-06 19:03:35 -0600

    DOM Manipulation

--- JS-Fundamentals/Project-06-DOM-Events/mentee/04-creating-and-removing-elements.js
+++ JS-Fundamentals/Project-06-DOM-Events/mentee/04-creating-and-removing-elements.js
@@ -2,8 +2,30 @@
 // 🎯 Topic: Creating & Removing Elements — createElement, append, remove
 // ------------------------------------------------------------
 
-// EX1: Create a new <li> with text 'New item' and append to a <ul>.
+// SETUP:
+// Create a <ul> element and append it to the document body.
+// This list will be used in EX1 to append <li> items.
+
+// EX1: Create a new <li> with text 'New item' and append to the <ul>.
 // EX2: Create an <img> with setAttribute('alt', 'avatar') and append to body.
 // EX3: Create a <div> with class 'card' and text, then append to body.
 // EX4: Remove the <img> from the page using element.remove().
 // EX5: In a comment, explain the difference between appendChild and append.
+
+const ul = document.createElement("ul");
+document.body.appendChild(ul);
+const li = document.createElement("li");
+li.textContent = "New Item";
+ul.appendChild(li);
+
+const image = document.createElement("img");
+image.src = "";
+image.setAttribute("alt", "avatar");
+document.body.appendChild(image);
+
+const div1 = document.createElement("div");
+div1.className = "card";
+div1.textContent = "card";
+document.body.appendChild(div1);
+
+image.remove();
--- JS-Fundamentals/Project-06-DOM-Events/mentee/05-event-listeners.js
+++ JS-Fundamentals/Project-06-DOM-Events/mentee/05-event-listeners.js
@@ -7,3 +7,33 @@
 // EX3: Add a 'keydown' listener on window that logs pressed key.
 // EX4: Inside a click handler, toggle a class on an element to show/hide it.
 // EX5: In a comment, explain why we use addEventListener instead of onclick= in HTML.
+
+const button = document.createElement("button");
+button.textContent = "click me";
+button.className = "whatever";
+button.addEventListener("click", () => {
+  console.log("Click!!!!!");
+});
+document.body.appendChild(button);
+
+const input = document.createElement("input");
+input.placeholder = "type here";
+document.body.appendChild(input);
+input.addEventListener("input", (e) => {
+  console.log(e.target.value);
+});
+
+window.addEventListener("keydown", (key) => {
+  console.log(key.key);
+});
+
+const div = document.createElement("div");
+div.textContent = "show / hide";
+// div.className = "idk";
+div.style.border = "1px solid black";
+document.body.appendChild(div);
+
+button.addEventListener("click", () => {
+  div.classList.toggle("hidden");
+  console.log(div.className);
+});
