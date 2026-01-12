50fb8546001020646ef97ce3bc2b0ba37d465329
Jocelin Arriaga <jocelin.arriaga@yahoo.com>
2026-01-12 07:26:11 -0600

    array methods

--- JS-Fundamentals/Project-07-Array-Methods/mentee/01-map.js
+++ JS-Fundamentals/Project-07-Array-Methods/mentee/01-map.js
@@ -7,3 +7,47 @@
 // EX3: Given prices [99,149], map to strings like "$99".
 // EX4: Given words ["hi","there"], map to word lengths.
 // EX5: Given objects [{n:2},{n:3}], map to squares [4,9] using the n property.
+
+
+const oddNumbers = [1,2,3];
+const evenNumbers = oddNumbers.map((number) => {
+    return number * 2
+});
+console.log(oddNumbers);
+console.log(evenNumbers);
+
+const names = ["jocelin","aimee"];
+const caps = names.map((name) => {
+    return name[0].toUpperCase() + name.slice(1)
+})
+
+console.log(caps);
+
+const prices = [99.124, 320.232]
+
+const convPrices = prices.map((price)=>{
+    return `$${(price)}`
+})
+
+console.log(convPrices)
+
+// console.log(prices.map(p => `${p}`))
+
+const words = ["hi","there"];
+const wordsLength = words.map((word) => word.length);
+console.log(wordsLength);
+
+
+const object = [{n:2},{n:3}];
+// const objsqr = object.map((obj) => {
+//     return obj.n * obj.n
+// });
+
+// console.log(objsqr);
+
+// obj.n.Mathpow()
+
+const objsqr = object.map((obj)=>{
+    return Math.pow(obj.n,2)
+})
+console.log(objsqr)
--- JS-Fundamentals/Project-07-Array-Methods/mentee/02-filter.js
+++ JS-Fundamentals/Project-07-Array-Methods/mentee/02-filter.js
@@ -7,3 +7,14 @@
 // EX3: Keep words with length >= 4 from ["js","code","array","map"].
 // EX4: Keep objects with inStock true from [{name:"A",inStock:true},...].
 // EX5: Keep emails that include "@gmail.com".
+
+
+
+const num = [1,2,3,4,5,6]
+
+const wat = num.filter((n)=>n%2 ===0)
+console.log(wat)
+
+const prices2 = [50,120,80,200];
+const lowPrices = prices2.filter((price) => price < 100);
+console.log(lowPrices);
